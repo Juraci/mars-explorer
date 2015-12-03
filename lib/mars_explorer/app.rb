19a0070264897fb87cef3bd52c30987bcb01cb7a
Juraci Vieira <juraci.vieira@gmail.com>
2015-12-03 13:30:10 -0200

    Making the moviment scenario pass, still missing refactoring

--- lib/mars_explorer/app.rb
+++ lib/mars_explorer/app.rb
@@ -1,7 +1,36 @@
 module MarsExplorer
   class App < Sinatra::Base
     post '/rest/mars/:command' do |command|
-     '(2, 0, S)' 
+
+      x = 0
+      y = 0
+      nose = 'N'
+
+      command.each_char do |c|
+        if c == 'M'
+          if nose == 'N'
+            y += 1
+          elsif nose == 'E'
+            x += 1
+          elsif nose == 'S'
+            y -= 1
+          elsif nose == 'W'
+            x -= 1
+          end
+        elsif c == 'R'
+          if nose == 'N'
+            nose = 'E'
+          elsif nose == 'E'
+            nose = 'S'
+          elsif nose == 'S'
+            nose = 'W'
+          elsif nose == 'W'
+            nose = 'N'
+          end
+        end
+      end
+
+      "(#{x}, #{y}, #{nose})"
     end
   end
 end
--- spec/integration/mars_explorer_spec.rb
+++ spec/integration/mars_explorer_spec.rb
@@ -3,6 +3,15 @@ require 'spec_helper'
 describe 'Mars Explorer API' do
 
   describe 'POST /rest/mars/' do
+    context 'when a moviment command is issued' do
+      it 'returns (0, 2, N)' do
+        post '/rest/mars/MM'
+
+        expect(last_response.status).to eq 200 
+        expect(last_response.body).to eq '(0, 2, N)'
+      end
+    end
+
     context 'when posting a rotation command' do
       it 'returns (2, 0, S )' do
         post '/rest/mars/MMRMMRMM'
