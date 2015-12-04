999d3ad75d61d18b5d811d5f010fae8ade05138d
Juraci Vieira <juraci.vieira@gmail.com>
2015-12-03 23:41:41 -0200

    Adding an scenario to verify the movement to the left

--- spec/integration/mars_explorer_spec.rb
+++ spec/integration/mars_explorer_spec.rb
@@ -3,21 +3,21 @@ require 'spec_helper'
 describe 'Mars Explorer API' do
 
   describe 'POST /rest/mars/:command' do
-    context 'when posting a moviment command' do
-      it 'returns (0, 2, N)' do
-        post '/rest/mars/MM'
-
-        expect(last_response.status).to eq 200 
-        expect(last_response.body).to eq '(0, 2, N)'
-      end
-    end
-
     context 'when posting a rotation command' do
       it 'returns (2, 0, S)' do
         post '/rest/mars/MMRMMRMM'
 
-        expect(last_response.status).to eq 200 
+        expect(last_response.status).to eq 200
         expect(last_response.body).to eq '(2, 0, S)'
+      end
+    end
+
+    context 'when posting a left moviment command' do
+      it 'returns (0, 2, W)' do
+        post '/rest/mars/MML'
+
+        expect(last_response.status).to eq 200
+        expect(last_response.body).to eq '(0, 2, W)'
       end
     end
   end
